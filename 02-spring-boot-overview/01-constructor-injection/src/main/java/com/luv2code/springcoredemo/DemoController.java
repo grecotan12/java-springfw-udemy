33ef2cb8f3673b42eccdb7570c84fccf2e73440c
tan nguyen <bivani2805@gmail.com>
2024-12-28 08:31:33 +0000

    Finish section 2 spring

---
+++ 02-spring-boot-overview/01-constructor-injection/src/main/java/com/luv2code/springcoredemo/DemoController.java
@@ -0,0 +1,25 @@
+package com.luv2code.springcoredemo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RestController
+public class DemoController {
+    
+    // define a private field for the dependency
+    private Coach myCoach;
+
+    // define constructor for dependency injection
+    @Autowired
+    public DemoController(Coach theCoach) {
+        myCoach = theCoach;
+    }
+
+    @GetMapping("/dailyworkout")
+    public String getDailyWorkout() {
+        return myCoach.getDailyWorkout();
+    }
+    
+
+}
